2e5e58bb7e6f00c68d92039b9e9d26d9113ccc42
Saheeda M Muhammed <164980128+SaheedaMMuhammed@users.noreply.github.com>
2026-01-27 15:22:32 +0530

    Time: 1 ms (71.14%) | Memory: 42.9 MB (37.88%) - LeetSync

--- 389-find-the-difference/find-the-difference.cs
+++ 389-find-the-difference/find-the-difference.cs
@@ -1,12 +1,10 @@
 public class Solution {
     public char FindTheDifference(string s, string t) 
     {
-        
-        if(s.Length==0)
-        {
-                return t[0];
-        }
-        int sumS=0;
+        int sumS=0;                             //all char have their corresponding 
+                                                //integer code so difference of their sum
+                                                //of code will be the code of the added
+                                                //character
         int sumT=0;
         for(int i=0;i<s.Length;i++)
         {
